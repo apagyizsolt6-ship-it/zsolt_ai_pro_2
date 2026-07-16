e684a44470e38b2ee8e9fe889ed89242daa89733
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-07-16 22:13:23 +0200

    Update app_spacing.dart

--- lib/core/theme/app_spacing.dart
+++ lib/core/theme/app_spacing.dart
@@ -4,18 +4,15 @@
 /// File: app_spacing.dart
 /// ===========================================
 
+library app_spacing;
+
 class AppSpacing {
   AppSpacing._();
 
   static const double xs = 4;
-
   static const double sm = 8;
-
   static const double md = 16;
-
   static const double lg = 24;
-
   static const double xl = 32;
-
   static const double xxl = 40;
 }
