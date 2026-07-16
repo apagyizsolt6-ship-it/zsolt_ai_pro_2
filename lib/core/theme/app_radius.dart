dda78ba594f83f3738241d46b864835b9f177174
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-07-16 22:25:19 +0200

    Create app_radius.dart

---
+++ lib/core/theme/app_radius.dart
@@ -0,0 +1,16 @@
+/*
+===========================================
+ZSOLT AI PRO
+Version: v1.0.0
+File: app_radius.dart
+===========================================
+*/
+
+class AppRadius {
+  AppRadius._();
+
+  static const double sm = 8;
+  static const double md = 16;
+  static const double lg = 24;
+  static const double xl = 32;
+}
