41fc90b924c9fac2261994b46446ca9ff39092a9
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-07-17 00:06:07 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
 void main() {
   runApp(const ZsoltAiProApp());
-}
-
-class ZsoltAiProApp extends StatelessWidget {
-  const ZsoltAiProApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ZSOLT AI PRO',
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            'ZSOLT AI PRO',
-            style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
